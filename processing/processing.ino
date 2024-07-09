bf03f4dbb199546193d98d53a081f56ee35031d0
Lumination Labs <code@luminationlabs.com>
2024-07-09 12:59:06 -0600

    Run processing example on core 0

--- processing/processing.ino
+++ processing/processing.ino
@@ -1,17 +1,17 @@
+#include <AudioOutputI2S.h>
 #include <WiFi.h>
 #include <driver/i2s.h>
-#include <AudioOutputI2S.h>
 
 #define VERBLIB_IMPLEMENTATION
 #include "verblib.h"
 
-// To use this example, you must connect an audio input source (e.g. microphone, line-in) and an audio
-// output device to Sonatino using a 3.5mm TRRS cable. A headset device with a microphone is a good
-// choice, but you can also use a TRRS adapter to connect separate audio devices.
+// To use this example, you must connect an audio input source (e.g. microphone,
+// line-in) and an audio output device to Sonatino using a 3.5mm TRRS cable. A
+// headset device with a microphone is a good choice, but you can also use a
+// TRRS adapter to connect separate audio devices.
 
-// Be sure to read the Sonatino documentation and set the board's level selection DIP switches
-// appropriately for your input and output levels.
-
+// Be sure to read the Sonatino documentation and set the board's level
+// selection DIP switches appropriately for your input and output levels.
 
 #define BUFFER_SAMPLES 50
 #define CHANNELS 2
@@ -31,6 +31,41 @@ int16_t input_buffer[BUFFER_SAMPLES];
 float filter_buffer[BUFFER_SAMPLES * CHANNELS];
 verblib rv;
 
+void processAudio(void *pvParameters) {
+  size_t bytes_read = 0;
+  float input_sample;
+  int16_t out_sample[2];
+
+  while (1) {
+    i2s_read(I2S_NUM_1, input_buffer, BUFFER_SAMPLES * sizeof(int16_t),
+             &bytes_read, portMAX_DELAY);
+
+    if (bytes_read > 0) {
+      // Read mono samples and copy into the stereo filter_buffer
+      for (uint32_t i = 0; i < bytes_read / sizeof(int16_t); i++) {
+        input_sample =
+            (float)input_buffer[i] / (float)std::numeric_limits<int16_t>::max();
+
+        filter_buffer[i * CHANNELS] = input_sample;
+        filter_buffer[i * CHANNELS + 1] = input_sample;
+      }
+
+      if (USE_REVERB) {
+        verblib_process(&rv, filter_buffer, filter_buffer, bytes_read / 2);
+      }
+
+      // Send samples to output
+      for (uint32_t i = 0; i < bytes_read / sizeof(int16_t) * CHANNELS;
+           i += CHANNELS) {
+        out_sample[0] =
+            filter_buffer[i] * (float)std::numeric_limits<int16_t>::max();
+        out_sample[1] =
+            filter_buffer[i + 1] * (float)std::numeric_limits<int16_t>::max();
+        dac->ConsumeSample(out_sample);
+      }
+    }
+  }
+}
 
 void setup() {
   // "USB CDC On Boot" must be enabled
@@ -83,34 +118,10 @@ void setup() {
     }
   }
 
+  // Start audio processing task
+  xTaskCreatePinnedToCore(processAudio, "processAudio", 2048, NULL, 1, NULL, 0);
+
   Serial.println("Ready");
 }
 
-void loop() {
-  size_t bytes_read = 0;
-  i2s_read(I2S_NUM_1, input_buffer, BUFFER_SAMPLES * sizeof(int16_t), &bytes_read, portMAX_DELAY);
-
-  if (bytes_read > 0) {
-    // Read mono samples and copy into the stereo filter_buffer
-    for (uint32_t i = 0; i < bytes_read / sizeof(int16_t); i++) {
-      float input_sample = (float)input_buffer[i] / (float)std::numeric_limits<int16_t>::max();
-
-      filter_buffer[i * CHANNELS] = input_sample;
-      filter_buffer[i * CHANNELS + 1] = input_sample;
-    }
-
-    if (USE_REVERB) {
-      verblib_process(&rv, filter_buffer, filter_buffer, bytes_read / 2);
-    }
-
-    // Send samples to output
-    for (uint32_t i = 0; i < bytes_read / sizeof(int16_t) * CHANNELS; i += CHANNELS) {
-      int16_t out_sample[2] = {
-        filter_buffer[i] * (float)std::numeric_limits<int16_t>::max(),
-        filter_buffer[i + 1] * (float)std::numeric_limits<int16_t>::max()
-      };
-      dac->ConsumeSample(out_sample);
-    }
-
-  }
-}
+void loop() {}
